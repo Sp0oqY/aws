77e8bb53d3d02e66e87ed70cf2277be9a3035d56
Sp0oqY <kastierm45@gmail.com>
2024-04-23 18:33:40 +0200

    trying smth

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-import awsExports from './Configuration';
+import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
 export default function App() {
